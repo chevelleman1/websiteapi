a04e0f6320926cdb0588afc684d9f2d38283515c
chevelleman1 <chevelleman1@hotmail.com>
2026-03-13 20:28:30 -0700

    Change API key header from 'x-api-key' to 'x-internal-key'

--- src/index.ts
+++ src/index.ts
@@ -34,7 +34,7 @@ app.use(limiter);
 // API Key authentication middleware
 const authenticateApiKey = (req: Request, res: Response, next: NextFunction) => {
   console.log(req);
-  const providedKey = req.headers['x-api-key'] as string;
+  const providedKey = req.headers['x-internal-key'] as string;
   const referer = req.headers.referer || req.headers.origin;
   const ALLOWED_DOMAINS = ["https://bobvel.sytes.net/", "http://localhost:8081/"]  
 
